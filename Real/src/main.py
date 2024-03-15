2a7fcbe53c7b1f5232356b03d818e3c026bde451
perryrobotics <47753315+perryrobotics@users.noreply.github.com>
2024-03-15 13:22:01 -0500

    Update main.py
    Comments revised.

--- Real/src/main.py
+++ Real/src/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+# Import all necessary libraries
 import os, sys
 from Constants import *
 from Sensors import *
@@ -10,20 +10,14 @@ import threading
 sys.path.append("/usr/lib")
 import _kipr as KIPR
 
-
-#sys.stdout = os.fdopen(sys.stdout.fileno(), "w", 0)
+# Keeps a motor static at a position when the function is called
 def motor_hold_position(motor,pos):
 	global thread_running
 	print("In the thread")
 	sys.stdout.flush()
 	thread_running = True
-    
 	KIPR.cmpc(motor)
-	#si = 1
-	#if pos < 0:
-	#	si=-1
-	#print (si)
-    
+	
 	while thread_running == True:
 		#print(KIPR.gmpc(motor))
 		if KIPR.gmpc(motor)<pos-10:
@@ -35,27 +29,7 @@ def motor_hold_position(motor,pos):
 	print("Claw thread exiting!")
 	sys.stdout.flush()
 	KIPR.motor(motor,0)
-        
-
-"""
-x = threading.Thread(target = motor_hold_position, args=(CLAW_MOTOR, 250), daemon = True)
-x.start()
-time.sleep(5)
-thread_running = False
-
-x = threading.Thread(target = motor_hold_position, args=(CLAW_MOTOR,-250), daemon = True)
-x.start()
-time.sleep(10)
-thread_running = False
-
-
-
-
-backward(1000,6000)
-"""
 KIPR.enable_servos()
-#start position
-
 
 #POSSIBLE-FLICK SWITCH FIRST
 '''
@@ -70,36 +44,17 @@ forward(900, 700)
 right(700, 700)
 move_servo_slow(ELBOW_SERVO, 1940, 30)
 left(700, 700)
-move_servo_slow(ARM_SERVO, 1100, 8)			#FLICKED THE SWITCH
+move_servo_slow(ARM_SERVO, 1100, 8)			
 '''
 
-
-
-
-"""
-print("Press a to start the chg. diabetic!!")
-while KIPR.a_button() == 0:
-	pass
-KIPR.msleep(2000) 
-"""       
-
-'''
-x = threading.Thread(target = motor_hold_position, args=(CLAW_MOTOR,250), daemon = True)
-x.start()
-time.sleep(5)
-thread_running = False
-'''
-
-
-#OG-DOES ASTRONAUTS FIRST
-#PUT SERVO START POSITIONS HERE
-#'''
+# Servo start positions initialized
 move_servo_slow(ARM_SERVO, 230, 50)
 move_servo_slow(ELBOW_SERVO, 100, 50)
 move_servo_slow(1, 1720, 50)
 
 
 #====================================================================================================
+
 #Go Play Botball!!
 x = threading.Thread(target = motor_hold_position, args=(CLAW_MOTOR,-250), daemon = True)
 x.start()
@@ -113,29 +68,32 @@ thread_running = False
 move_servo_slow(ELBOW_SERVO, 1430, 50)
 move_servo_slow(ARM_SERVO, 230, 50)
 
-#MOVE FORWARSD AND GET THE POEPLE WITH CLAW WHEN WE DO IT 
-#GO BCKWARDAS A LITTLE BIT
+# Mult-tasking via threading
 x = threading.Thread(target = motor_hold_position, args=(CLAW_MOTOR, 250), daemon = True)
 x.start()
 time.sleep(5)
 thread_running = False
 
+# Move forward to get the people in the claw
 move_servo_slow(ARM_SERVO, 2040, 5)
 backward(1200, 200)
-right(900,960)					#MAY HAVE TO CHANGE AT COMP
+right(900,960)					
 forward(1200, 2000)
 move_to_black(900)
-right(900, 980)				#MAY HAVE TO CHANGE AT COMP
+right(900, 980)				
 forward(1200, 3000)
-forward(700, 1200)  #ALIGN TO 1st PIPE
+
+# Align to the 1st pipe
+forward(700, 1200)  
 backward(1200, 2800)
-left(1000, 1325)					#MAY AHVE TO CHANGE AT COMP A LOT
+left(1000, 1325)					
 move_servo_slow(ARM_SERVO, 1750, 10)
 backward(1200, 2300)
 backward(500, 190)
-move_servo_slow(1, 1150, 50)  #GOT THE CUP, NOW GET OUT
+move_servo_slow(1, 1150, 50)  
 left(900, 10)
 
+# Align small bot in front of airlock
 forward(1200, 1300)
 move_servo_slow(1, 2040, 10)
 right(900, 2230)
@@ -145,24 +103,28 @@ move_servo_slow(ELBOW_SERVO, 1940, 10)
 forward(600, 1200)
 left(200, 50)
 
+
 x = threading.Thread(target = motor_hold_position, args=(CLAW_MOTOR,-150), daemon = True)
 x.start()
 time.sleep(5)
 thread_running = False
 
-#MOVE CLAW DOWN AND DROP ASTRONAUTS
+# Move claw down and drop the astronauts in the airlock
 x = threading.Thread(target = motor_hold_position, args=(CLAW_MOTOR, 150), daemon = True)
 x.start()
 time.sleep(5)
 thread_running = False
 
+# Alignment with the airlock switch
 backward(1200, 670)
 right(500, 220)
 forward(1200,2000)
 move_servo_slow(ARM_SERVO, 800, 10)
 forward(900, 740)
 left(500, 140)
-move_servo_slow(ARM_SERVO, 1000, 10)		#THIS FLICKS THE SWITCH, CHANGE FOR BIG ROBOT
+
+# Flicks the airlock switch so big bot recieves the purple noodle
+move_servo_slow(ARM_SERVO, 1000, 10)		
 right(1000, 600)
 move_servo_slow(1, 300, 50)
 x = threading.Thread(target = motor_hold_position, args=(CLAW_MOTOR,-350), daemon = True)
@@ -170,6 +132,7 @@ x.start()
 time.sleep(5)
 thread_running = False
 
+# Gathering rocks and sorting
 backward(1200, 300)
 move_servo_slow(ELBOW_SERVO, 1196, 50)
 move_servo_slow(ARM_SERVO, 20, 50)
@@ -186,10 +149,7 @@ forward(1200, 3000)
 thread_running = False
 tDriftLeftE(1200, 400)
 forward(1200, 800)
-
 move_servo_slow(ARM_SERVO, 1500, 50)
 move_servo_slow(ELBOW_SERVO, 800, 50)
 deadRight(1200, 600)
 backward
-
-#'''
