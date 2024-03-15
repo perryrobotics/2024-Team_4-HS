bb00bda1df6eea0e22c8b9fd049a703b2eacf746
perryrobotics <47753315+perryrobotics@users.noreply.github.com>
2024-03-15 13:32:27 -0500

    Update Effectors.py
    Comments revised.

--- Real/src/Effectors.py
+++ Real/src/Effectors.py
@@ -9,7 +9,7 @@ from math import *
 sys.path.append("/usr/lib")
 import _kipr as KIPR
 
-    
+# Slows servo movement
 def move_servo_slow(port, end_pos, step):
 	start_pos = KIPR.get_servo_position(port)
 	if start_pos > end_pos:
@@ -19,20 +19,20 @@ def move_servo_slow(port, end_pos, step):
 		KIPR.set_servo_position(port, pos)
 		KIPR.msleep(25)
 	KIPR.set_servo_position(port, end_pos)
-            
+
 def arm_down(step):
 	move_servo_slow(ARM_SERVO,ARM_DOWN, step)
-        
+
 def arm_back(step):
 	move_servo_slow(ARM_SERVO,ARM_BACK, step)
-        
+
 def arm_forward(step):
 	move_servo_slow(ARM_SERVO,ARM_FORWARD, step)
 	#ove_servo_slow(ELBOW_SERVO,ELBOW_FORWARD, step)
-        
+
 def elbow_down(step):
 	move_servo_slow(ELBOW_SERVO,ELBOW_DOWN, step)
-        
+
 def elbow_forward(step):
 	move_servo_slow(ELBOW_SERVO,ELBOW_FORWARD, step)
 	       
